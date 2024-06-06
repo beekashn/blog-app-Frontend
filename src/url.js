e5edec4023179ff903ffb12cf44dfd98f8b2e1e3
beekashn <git7beekash7@gmail.com>
2024-06-06 18:09:14 +0545

    images url render

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
 export const URL = "https://blog-app-l3cj.onrender.com";
-export const IMAGE_FOLDER = "http://localhost:5000/images/";
+export const IMAGE_FOLDER = "https://blog-app-l3cj.onrender.com/images/";
